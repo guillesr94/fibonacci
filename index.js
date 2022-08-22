5ea0a854216dca9a4a6423c646af416ca05efd34
Guillermo Suarez Reinoso <81838049+guillesr94@users.noreply.github.com>
2022-08-22 20:31:58 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+// - Una función que admita un parámetro "num", y devuelva una lista con esa cantidad de números de la secuencia de Fibonacci (Por ejemplo: num = 6 => Resultado [1, 1, 2, 3, 5, 8])
+
+let array=[0,1]
+
+fibonacci= (num)=>{
+  for(let i=2; i<=num; i++){
+    array[i]=array[i-2]+array[i-1]
+  }
+  return array
+}
+console.log(fibonacci(10));
+
+
+// - Ejecuta la depuración de VSCode para visualizar la ejecución de la función
+
